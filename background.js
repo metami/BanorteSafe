1a9da7b0de79ce5261698ec96ee9d19153b700ec
metami <36749040+metami@users.noreply.github.com>
2019-08-25 11:29:21 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,36 @@
+
+
+
+window.onload = function()
+{
+    
+chrome.browserAction.setIcon({path:"icon16.png"});
+chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    
+
+var url= tabs[0].url;
+
+var origin = url.match(/^[\w-]+:\/{2,}\[?[\w\.:-]+\]?(?::[0-9]*)?/)[0];
+
+
+var anst= document.getElementById("ans")
+var yest= document.getElementById("yes")
+		var ban ="https://www.banorte.com";
+		var check = { active: true, currentWindow: true };
+		if(origin == ban)
+		{	
+		yest.innerHTML='<img src="yes.png" style="width:50%">'
+		 anst.innerHTML= "Sitio Seguro. <br> Usted se encuentra en un sitio perteneciente a Banorte";
+		
+		} 
+		else
+		{
+		yest.innerHTML='<img src="no.png" style="width:50%">'
+		anst.innerHTML= "Este sitio web no pertenece a Banorte. <br>Su informacion esta en riesgo.";
+		
+		
+		}
+
+});
+	
+}	;
